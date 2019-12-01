fede4a38a280f2938947fd1ed3e89d5d5dc1fa4c
Ahmed Emad El-Agroudy <ahmed.3mad.eldin@gmail.com>
2019-12-01 22:58:22 +0200

    got latest inquireis page to work

--- inquireis.php
+++ inquireis.php
@@ -11,9 +11,9 @@
     echo "<br>
     <h1 style='color:#DAA520'>Latest Inquireis!</h1>
     <br><br>" ;
-    for($i=$DB->numRows(); $i>0; $i--){
+    for($i=$DB->numRows(); $i>0; --$i){
     $x=$DB->getdata();
-    echo "Message ID: " . $x[$i]->id . "<br>Requested by: " . $x[$i]->requester_name . "<br>Requester email: " . $x[$i]->requester_email . "<br><br>" . "Message subject: " . $x[$i]->subject . "<br>Message content: " . $x[$i]->message . "<br>";
+    echo "<hr><br>" . "<b>Message ID: </b>" . $x[$i-1]->id . "<br><b>Requested by: </b>" . $x[$i-1]->requester_name . "<br><b>Requester email: </b>" . $x[$i-1]->requester_email . "<br><br>" . "<b>Message subject: </b>" . $x[$i-1]->subject . "<br><b>Message content: </b>" . $x[$i-1]->message . "<br>";
     echo "<br><br>";
     }
     ?>

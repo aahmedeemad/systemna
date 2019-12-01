c6af90b55eac172d0afff4165e3e083b5b0ccb9a
Ahmed Emad El-Agroudy <ahmed.3mad.eldin@gmail.com>
2019-12-01 22:22:55 +0200

    updated inquireis view page with latest bugs to fix later when islam replies

--- inquireis.php
+++ inquireis.php
@@ -11,11 +11,11 @@
     echo "<br>
     <h1 style='color:#DAA520'>Latest Inquireis!</h1>
     <br><br>" ;
-    for($i=$DB->numRows(); $i>=0; --$i){
+    for($i=$DB->numRows(); $i>0; $i--){
     $x=$DB->getdata();
-    echo "<h2> Message ID: " . $x[$i]->id . "<br>" . "Requested by: " . $x[$i]->requester_name . "</h2>" . "<br><h4>" . "Requester email:" . $x[$i]->requester_email . "</h4>" . "<br>" . "Message subject: " . $x[$i]->subject . "<br>" . "Message content: " . $x[$i]->message . "<br>";
-    }
+    echo "Message ID: " . $x[$i]->id . "<br>Requested by: " . $x[$i]->requester_name . "<br>Requester email: " . $x[$i]->requester_email . "<br><br>" . "Message subject: " . $x[$i]->subject . "<br>Message content: " . $x[$i]->message . "<br>";
     echo "<br><br>";
+    }
     ?>
 </div>
 

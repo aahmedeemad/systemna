ea9c1769bde16111a8526b466f999f88cde5cfd4
markRefaat <mark.refaat.ramzy@gmail.com>
2019-12-02 12:53:14 +0200

    Sidenav  links now working - add session_start in header

--- template/header.php
+++ template/header.php
@@ -1,4 +1,6 @@
 <?php 
+session_start();
+$SESSION['username']="Mark Refaat Ramzy";
 include('../DB/Database.php');
 $DB = new Database();
 ?>
@@ -10,7 +12,7 @@ $DB = new Database();
         <link rel="stylesheet" href="../css/style.css">
         <link rel="stylesheet" href="../css/all.min.css">
         <link rel="stylesheet" href="../css/popup.css">
-        <link rel="icon" type="image/png" href="logo.png">
+        <link rel="icon" type="image/png" href="../template/logo.png">
         <title>Admin Page</title>
     </head>
 
@@ -20,22 +22,26 @@ $DB = new Database();
                 <div class="navbar-toggle">
                     <i class="fas fa-bars"></i>
                 </div>
-                <div class="logo-name"><strong style="color: #DAA520">System</strong><strong>na</strong></div>
+                <a style="text-decoration: none;" href="../pages/index.php"><div class="logo-name"><strong style="color: #DAA520">System</strong><strong style="color: white">na</strong></div></a>
                 <div class="right">Logout</div>
             </header>
             <div class="mainPage">
                 <div class="sidenav">
-                    <div class="sidenav-header">
-                        <div class="avatar"><img src="../template/avatar.jpg" alt="" class="rounded-circle"></div>
-                        <div class="title">
-                            <h1 class="name">Mark</h1>
-                            <div class="position">Admin</div>
+                    
+                        <div class="sidenav-header">
+                            <div class="avatar"><img src="../template/avatar.jpg" alt="" class="rounded-circle"></div>
+                            <div class="title">
+                                <a style="text-decoration: none;" href="../pages/profile.php"><h1 class="name"><?php echo explode(" ", $SESSION['username'])[0] ?></h1></a>
+                                <div class="position">Admin</div>
+                            </div>
                         </div>
-                    </div>
                     <ul>
-                        <li class="sidenav-button"><a href="##"><i class='fas fa-home fa-sm icon-button'></i><span class="button-text"> Home</span></a></li>
-                        <li class="sidenav-button"><a href="#"><i class='fas fa-users fa-sm icon-button'></i><span class="button-text"> Pending Members</span></a></li><li class="sidenav-button"></li>
-                        <li class="sidenav-button"><a href="#"><i class='fas fa-users fa-sm icon-button'></i><span class="button-text"> Pending Members</span></a></li><li class="sidenav-button"></li>
+                        <li class="sidenav-button"><a href="../pages/index.php"><i class='fas fa-home fa-sm icon-button'></i><span class="button-text"> Home</span></a></li>
+                        <li class="sidenav-button"><a href="../pages/AddQuestion.php"><i class='fas fa-users fa-sm icon-button'></i><span class="button-text"> Add Question</span></a></li><li class="sidenav-button"></li>
+                        <li class="sidenav-button"><a href="../pages/faq.php"><i class='fas fa-users fa-sm icon-button'></i><span class="button-text"> FAQ</span></a></li><li class="sidenav-button"></li>
+                        <li class="sidenav-button"><a href="../pages/lettertypes.php"><i class='fas fa-users fa-sm icon-button'></i><span class="button-text"> Letters</span></a></li><li class="sidenav-button"></li>
+                        <li class="sidenav-button"><a href="../pages/profile.php"><i class='fas fa-users fa-sm icon-button'></i><span class="button-text"> Profile</span></a></li><li class="sidenav-button"></li>
+                        <li class="sidenav-button"><a href="../pages/inquireis.php"><i class='fas fa-users fa-sm icon-button'></i><span class="button-text"> Inquireis</span></a></li><li class="sidenav-button"></li>
                     </ul>
                 </div>
                 <div class="content">

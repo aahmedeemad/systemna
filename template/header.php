755f3ce9b937fc0d817790fbf09bae3266396974
Ahmed Emad El-Agroudy <ahmed.3mad.eldin@gmail.com>
2019-12-08 22:22:31 +0200

    removed notifications from admin and added waiting users counter

--- template/header.php
+++ template/header.php
@@ -20,6 +20,17 @@ $DB = new Database();
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
         <script>
             $(document).ready(function () {
+                $('#usrs_Counter')
+                    .text('<?php
+                          $sql = "
+                        SELECT *
+                        FROM employee
+                        WHERE accepted = 0
+                        ";
+                          $DB->query($sql);
+                          $DB->execute();
+                          echo ($DB->numRows());
+                          ?>');
                 $('#noti_Counter')
                     .text('<?php
                           $uid = $_SESSION['id'];
@@ -137,15 +148,8 @@ $DB = new Database();
                     <ul>
                         <li class="sidenav-button"><a href="<?php echo $_SESSION['type']=='admin' ? 'index.php' : '../pages/lettertypes.php'  ?>"><i class='fas fa-home fa-sm icon-button'></i><span class="button-text"> Home</span></a></li>
                         <li class="sidenav-button"><a href="../pages/profile.php"><i class='fas fa-user fa-sm icon-button'></i><span class="button-text"> Profile</span></a></li><li class="sidenav-button"></li>
-                        <li class="sidenav-button" id="noti_Container"><a id="noti_Button"><i class='fas fa fa-bell fa-sm icon-button'></i><span class="button-text"> Notifications
-                            <div id="noti_Counter"></div><!--SHOW NOTIFICATIONS COUNT.-->
-                            <div id="notifications"><!--THE NOTIFICAIONS DROPDOWN BOX.-->
-                                <div id="notidata"></div>
-                                <div id="markAll">Mark All as Read</div>
-                            </span>
-                            </span></a></li>
                         <li class="sidenav-button"><a href="../pages/AddQuestion.php"><i class='fas fa-question fa-sm icon-button'></i><span class="button-text"> Add Question</span></a></li><li class="sidenav-button"></li>
-                        <li class="sidenav-button"><a href="../pages/waitingUsers.php"><i class='fas fa-clock fa-sm icon-button'></i><span class="button-text"> Waiting Users</span></a></li><li class="sidenav-button"></li>
+                        <li class="sidenav-button"><a href="../pages/waitingUsers.php"><i class='fas fa-clock fa-sm icon-button'></i><span class="button-text"> Waiting Users <div id="usrs_Counter"></div> </span></a></li><li class="sidenav-button"></li>
                         <li class="sidenav-button"><a href="../pages/faq.php"><i class='fas fa-question-circle fa-sm icon-button'></i><span class="button-text"> FAQ</span></a></li><li class="sidenav-button"></li>
                     </ul>
                 </div>
@@ -163,16 +167,13 @@ $DB = new Database();
                     <ul>
                         <li class="sidenav-button"><a href="<?php echo $_SESSION['type']=='admin' ? 'index.php' : '../pages/lettertypes.php'  ?>"><i class='fas fa-home fa-sm icon-button'></i><span class="button-text"> Home</span></a></li>
                         <li class="sidenav-button"><a href="../pages/profile.php"><i class='fas fa-user fa-sm icon-button'></i><span class="button-text"> Profile</span></a></li><li class="sidenav-button"></li>
-                        <li class="sidenav-button" id="noti_Container">
-                            <a id="noti_Button"><i class='fas fa fa-bell  fa-sm icon-button'></i><span class="button-text"> Notifications
-                                <div id="noti_Counter"></div><!--SHOW NOTIFICATIONS COUNT.-->
-                                <div id="notifications"><!--THE NOTIFICAIONS DROPDOWN BOX.-->
-                                    <div id="notidata"></div>
-                                    <div id="markAll">Mark All as Read</div>
-                                </div>
-                                </span>
-                            </a>
-                        </li>
+                        <li class="sidenav-button" id="noti_Container"><a id="noti_Button"><i class='fas fa fa-bell  fa-sm icon-button'></i><span class="button-text"> Notifications
+                            <div id="noti_Counter"></div><!--SHOW NOTIFICATIONS COUNT.-->
+                            <div id="notifications"><!--THE NOTIFICAIONS DROPDOWN BOX.-->
+                                <div id="notidata"></div>
+                                <div id="markAll">Mark All as Read</div>
+                            </div>
+                        </span></a></li>
                         <li class="sidenav-button"><a href="../pages/lettertypes.php"><i class='fas fa-envelope fa-sm icon-button'></i><span class="button-text"> Request Letters</span></a></li><li class="sidenav-button"></li>
                         <li class="sidenav-button"><a href="../pages/viewRequest.php"><i class='fas fa-clock fa-sm icon-button'></i><span class="button-text"> View Requests</span></a></li><li class="sidenav-button"></li>
                         <li class="sidenav-button"><a href="../pages/faq.php"><i class='fas fa-question-circle fa-sm icon-button'></i><span class="button-text"> FAQ</span></a></li><li class="sidenav-button"></li>

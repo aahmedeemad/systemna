4c9140f8e066f27f7527a22da4f517d78d680d00
Ahmed Emad El-Agroudy <ahmed.3mad.eldin@gmail.com>
2019-12-18 11:56:11 +0200

    changed the edit/add button

--- pages/viewFAQ.php
+++ pages/viewFAQ.php
@@ -5,7 +5,6 @@ include "../template/header.php";
 <?php if(!isset($_SESSION['username'])){header('Location:../index.php');}
  if($_SESSION['type']=='user'){header('Location:lettertypes.php');}    ?>
 
-<br>
 <div style="text-align: center; align-self: center;">
 <div class="pages_edit" id="faq_add" onclick="addFAQ.send()"></div>
 </div>

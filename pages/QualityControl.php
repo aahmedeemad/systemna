a725218666d710c413402c2ae59f52ffd945a847
Abanoub George <abanoub-g-lamie@hotmail.com>
2019-12-21 17:55:15 +0200

    Qc bysma3 fel db

--- pages/QualityControl.php
+++ pages/QualityControl.php
@@ -84,9 +84,17 @@ include "../template/header.php";
                 <td>{$Boolsalray}</td>
                 <td>{$date}</td>
                 <td>{$BoolPriority}</td>
-                <td><input type='text'placeholder='Write your comment here...'size='30'><input type='submit' value='Submit Comment'>
-                </td>";
-                ?></tr>
+                ";
+            
+                ?>
+                <td class ="Comment">
+                <form action="../operations/AddComment.php?Request_id=<?php echo $x[$i]->Request_id?>" method="post">
+                <input type='text' name="Comment" placeholder='Write your comment here...'size='30'>
+                <br>
+                <input type='submit'onclick ="return alert ('Your Comment has been added ')" value="Submit Comment" name="AddC">
+                </form>    
+                </td>
+                </tr>
     <?php
         }
     }

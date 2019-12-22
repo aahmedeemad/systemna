f14d92ad3112165e7d1918234c58fcffb3ddb164
Islam Magdy <DashaDeshozz@gmail.com>
2019-12-22 14:11:29 +0200

    forgotten delete button in faq table

--- operations/DeleteTable.php
+++ operations/DeleteTable.php
@@ -1,9 +1,19 @@
 <?php
 include "../DB/Database.php";
 $DB2 = new Database();
+if(isset($_GET['id'])){
 $d_id = $_GET['id'];
 $sql = "update employee set active=0 where id = '$d_id;'";
 $DB2->query($sql);
 $DB2->execute();
 header("Location: ../pages/index.php");
+}
+if(isset($_GET['qid'])){
+$qid = $_GET['qid'];
+$sql = "delete from faq where ID = '$qid';";
+$DB2->query($sql);
+$DB2->execute();
+header("Location: ../pages/viewFAQ.php");
+
+}
 ?>
--- pages/viewFAQ.php
+++ pages/viewFAQ.php
@@ -63,8 +63,8 @@ include "../template/header.php";
                 //<td ><div class='sal' id={$x[$i]->id}>{$salary}</div></td>";
                 ?>
     <td><a type='submit' href="EditFAQ.php?id=<?php echo $id ;?>" class='EditBtn'>Edit</a></td>
-    <td><a type='submit' onclick="return confirm('Delete this account?')"
-            href="../operations/DeleteTable.php?id=<?php echo $id ;?>" class='EditBtn'>Delete</a></td>
+    <td><a type='submit' onclick="return confirm('Delete this question?')"
+            href="../operations/DeleteTable.php?qid=<?php echo $id ;?>" class='EditBtn'>Delete</a></td>
 
     </tr>
     <?php

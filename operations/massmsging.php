912767fb4332b70df077aef8d96f08499cf14f04
Ahmed Emad El-Agroudy <ahmed.3mad.eldin@gmail.com>
2019-12-27 13:40:01 +0200

    added mail settings in external page

--- operations/massmsging.php
+++ operations/massmsging.php
@@ -2,20 +2,7 @@
 session_start();
 include('../DB/Database.php');
 $DB = new Database();
-
-use PHPMailer\PHPMailer\PHPMailer; /* Namespace alias. */
-use PHPMailer\PHPMailer\Exception; /* Namespace alias. */
-require '../composer/vendor/autoload.php'; /* Include the Composer generated autoload.php file. */
-date_default_timezone_set('Etc/UTC'); /* Set the script time zone to UTC. */
-$mail = new PHPMailer(TRUE); /* Create a new PHPMailer object. Passing TRUE to the constructor enables exceptions. */
-$mail->SMTPOptions = array('ssl'=>array('verify_peer'=>false, 'verify_peer_name'=>false, 'allow_self_signed'=>true));
-$mail->isSMTP(); /* Use SMTP. */
-$mail->Host = 'smtp.gmail.com'; /* Google (Gmail) SMTP server. */
-$mail->Port = 587; /* SMTP port. */
-$mail->SMTPAuth = true; /* Set authentication. */
-$mail->SMTPSecure = 'tls'; /* Set authentication. */
-$mail->Username = 'systemnamiu@gmail.com'; /* Username (email address). */
-$mail->Password = 'tpwvzpocyggbhmlr'; /* Google account password. */
+include('../template/mailsettings.php');
 $mail->setFrom('systemnamiu@gmail.com', $_SESSION["name"] . ' from SYSTEMNA'); /* Set the mail sender. */
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {

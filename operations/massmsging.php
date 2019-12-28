2161f5914cf3d103c716a26da731ce9e0b35ccee
Ahmed Emad El-Agroudy <ahmed.3mad.eldin@gmail.com>
2019-12-28 07:36:04 +0200

    added dynamic email template

--- operations/massmsging.php
+++ operations/massmsging.php
@@ -2,8 +2,22 @@
 session_start();
 include('../DB/Database.php');
 $DB = new Database();
-include('../template/mailsettings.php');
+use PHPMailer\PHPMailer\PHPMailer; /* Namespace alias. */
+use PHPMailer\PHPMailer\Exception; /* Namespace alias. */
+require '../composer/vendor/autoload.php'; /* Include the Composer generated autoload.php file. */
+date_default_timezone_set('Etc/UTC'); /* Set the script time zone to UTC. */
+$mail = new PHPMailer(TRUE); /* Create a new PHPMailer object. Passing TRUE to the constructor enables exceptions. */
+$mail->SMTPOptions = array('ssl'=>array('verify_peer'=>false, 'verify_peer_name'=>false, 'allow_self_signed'=>true));
+$mail->isSMTP(); /* Use SMTP. */
+$mail->Host = 'smtp.gmail.com'; /* Google (Gmail) SMTP server. */
+$mail->Port = 587; /* SMTP port. */
+$mail->SMTPAuth = true; /* Set authentication. */
+$mail->SMTPSecure = 'tls'; /* Set authentication. */
+$mail->Username = 'systemnamiu@gmail.com'; /* Username (email address). */
+$mail->Password = 'tpwvzpocyggbhmlr'; /* Google account password. */
 $mail->setFrom('systemnamiu@gmail.com', $_SESSION["name"] . ' from SYSTEMNA'); /* Set the mail sender. */
+$mail->CharSet = 'utf-8';
+$mail->isHTML(true);
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if ($_POST['type'] == "notiall")
@@ -46,10 +60,21 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $mailsubject = filter_var($_POST['mailsubject'], FILTER_SANITIZE_STRING);
             $mailcontent = filter_var($_POST['mailcontent'], FILTER_SANITIZE_STRING);
             $mail->Subject = "$mailsubject"; /* Set the subject. */
-            $mail->Body = "$mailcontent"; /* Set the mail message body. */
+            //$mail->Body = "$mailcontent"; /* Set the mail message body. */
             for ($i=0; $i<$y; $i++) {
                 $umail = $x[$i]->email;
                 $uname = $x[$i]->fullname;
+                $email_vars = array(
+                    'name' => $uname,
+                    'content' => $mailcontent,
+                );
+                $body = file_get_contents('../template/htmlemail.html');
+                if(isset($email_vars)){
+                    foreach($email_vars as $k=>$v){
+                        $body = str_replace('{'.strtoupper($k).'}', $v, $body);
+                    }
+                }
+                $mail->MsgHTML($body);
                 $mail->addAddress("$umail", "$uname"); /* Add a recipient. */
                 $mail->send(); /* Send the mail. */
                 $mail->ClearAddresses(); /* Removes the data after sending. */
@@ -69,7 +94,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $mailsubject = filter_var($_POST['mailsubject'], FILTER_SANITIZE_STRING);
             $mailcontent = filter_var($_POST['mailcontent'], FILTER_SANITIZE_STRING);
             $mail->Subject = "$mailsubject"; /* Set the subject. */
-            $mail->Body = "$mailcontent"; /* Set the mail message body. */
+            $email_vars = array(
+                'name' => $uname,
+                'content' => $mailcontent,
+            );
+            $body = file_get_contents('../template/htmlemail.html');
+            if(isset($email_vars)){
+                foreach($email_vars as $k=>$v){
+                    $body = str_replace('{'.strtoupper($k).'}', $v, $body);
+                }
+            }
+            $mail->MsgHTML($body);
+            //$mail->Body = "$mailcontent"; /* Set the mail message body. */
             $mail->addAddress("$umail", "$uname"); /* Add a recipient. */
             $mail->send(); /* Send the mail. */
         }
@@ -88,7 +124,18 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $mailcontent = $_POST['mailcontent'];
         $mail->setFrom('systemnamiu@gmail.com', 'From SYSTEMNA'); /* Set the mail sender. */
         $mail->Subject = "$mailsubject"; /* Set the subject. */
-        $mail->Body = "$mailcontent"; /* Set the mail message body. */
+        $email_vars = array(
+            'name' => $uname,
+            'content' => $mailcontent,
+        );
+        $body = file_get_contents('../template/htmlemail.html');
+        if(isset($email_vars)){
+            foreach($email_vars as $k=>$v){
+                $body = str_replace('{'.strtoupper($k).'}', $v, $body);
+            }
+        }
+        $mail->MsgHTML($body);
+        //$mail->Body = "$mailcontent"; /* Set the mail message body. */
         $mail->addAddress("$umail", "$uname"); /* Add a recipient. */
         $mail->send(); /* Send the mail. */
 

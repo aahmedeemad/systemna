ad3f9b2c17c058e24b6d2a6183ded6cec07194d8
markRefaat <mark.refaat.ramzy@gmail.com>
2019-12-31 14:37:19 +0200

    fix delete table page

--- operations/DeleteTable.php
+++ operations/DeleteTable.php
@@ -7,28 +7,28 @@ include "../DB/Database.php";
 $DB = new Database();
 if(isset($_GET['id'])){
     $d_id = $_GET['id'];
-    $sql = "update employee set active=0 where id = '$d_id;'";
+    $sql = "update employee set active=0 where id = '$d_id'";
     $DB->query($sql);
     $DB->execute();
 //    header("Location: ../pages/index.php");
 }   
 if(isset($_GET['wid'])){
     $d_id = $_GET['wid'];
-    $sql = "update employee set active=0 where id = '$d_id;'";
+    $sql = "update employee set active=0 where id = '$d_id'";
     $DB->query($sql);
     $DB->execute();
 //    header("Location: ../pages/waitingUsers.php");
 }
 else if(isset($_GET['qid'])){
     $qid = $_GET['qid'];
-    $sql = "delete from faq where ID = '$qid';";
+    $sql = "delete from faq where ID = '$qid'";
     $DB->query($sql);
     $DB->execute();
 //    header("Location: ../pages/viewFAQ.php");
 }
 else if(isset($_GET['lid'])){
     $lid = $_GET['lid'];
-    $sql = "delete from requests_types where type_id = '$lid';";
+    $sql = "delete from requests_types where type_id = '$lid'";
     $DB->query($sql);
     $DB->execute();
 //    header("Location: ../pages/allLetters.php");

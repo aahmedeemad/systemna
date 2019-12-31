7dcf7af2c5c4bfe56fdad08e05f8dc5c94938df9
Ahmed Emad El-Agroudy <ahmed.3mad.eldin@gmail.com>
2019-12-31 22:08:27 +0200

    added try and catch

--- operations/notiop.php
+++ operations/notiop.php
@@ -6,21 +6,35 @@ $DB = new Database(); /* Making a DB object */
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if ($_POST['type'] == "setnotidata")
     {
-        $uid = $_SESSION['id']; /* Getting the user ID */
-        $sql = "SELECT * FROM notifications WHERE userid = $uid AND status = 0 "; /* SQL query to get the data from the DB */
-        $DB->query($sql); /* Using the query function made in DB/Database.php */
-        $DB->execute(); /* Using the excute function made in DB/Database.php */
-        for($i=$DB->numRows(); $i>0; --$i){ /* iterating the results by the num of rows */
-            $x=$DB->getdata(); /* creates an array of the output result */
-            echo ($i) . "- " . $x[$i-1]->notidata . "<hr>"; /* Printing the output */
+        try {
+            $uid = $_SESSION['id']; /* Getting the user ID */
+            $sql = "SELECT * FROM notifications WHERE userid = $uid AND status = 0 "; /* SQL query to get the data from the DB */
+            $DB->query($sql); /* Using the query function made in DB/Database.php */
+            $DB->execute(); /* Using the excute function made in DB/Database.php */
+            for($i=$DB->numRows(); $i>0; --$i){ /* iterating the results by the num of rows */
+                $x=$DB->getdata(); /* creates an array of the output result */
+                echo ($i) . "- " . $x[$i-1]->notidata . "<hr>"; /* Printing the output */
+            }
+        }
+        catch(Exception $e)
+        {
+            echo "<div class='alert alert-danger'>Error please try again later</div>";
+            error_log("error while getting notifications data");
         }
     }
     else if ($_POST['type'] == "markread")
     {
-        $uid = $_SESSION['id']; /* Getting the user ID */
-        $sql = " UPDATE notifications SET status = 1 WHERE userid = $uid AND status = 0 "; /* SQL query to set the data into the DB */
-        $DB->query($sql); /* Using the query function made in DB/Database.php */
-        $DB->execute(); /* Using the excute function made in DB/Database.php */
+        try {
+            $uid = $_SESSION['id']; /* Getting the user ID */
+            $sql = " UPDATE notifications SET status = 1 WHERE userid = $uid AND status = 0 "; /* SQL query to set the data into the DB */
+            $DB->query($sql); /* Using the query function made in DB/Database.php */
+            $DB->execute(); /* Using the excute function made in DB/Database.php */
+        }
+        catch(Exception $e)
+        {
+            echo "<div class='alert alert-danger'>Error please try again later</div>";
+            error_log("error while setting notifications read");
+        }
     }
 }
 else 

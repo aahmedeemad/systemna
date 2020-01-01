32c016f1f2fd4f3762ecf37ba8c8839409fce301
Ahmed Emad El-Agroudy <ahmed.3mad.eldin@gmail.com>
2020-01-02 00:08:07 +0200

    better handling for massmsging errors

--- pages/massmsgs.php
+++ pages/massmsgs.php
@@ -1,7 +1,6 @@
 <?php 
 $pageTitle = "SYSTEMNA | Mass Messaging"; /* Setting the page title */
 include "../template/header.php"; /* Including the header file */
-try {
 ?>
 <style>
     @media screen and (max-width: 800px) {
@@ -11,6 +10,10 @@ try {
     }
 </style>
 <table style="width: 100%;">
+    <?php
+    if ($_SESSION['error'] == 'error in sql') {
+        echo "<br><div class='alert alert-danger' style='text-align: center;'>ERROR! Please try again later</div>";
+    } ?>
     <tr>
         <td>
             <div>
@@ -20,7 +23,7 @@ try {
             <fieldset>
                 <label>Select the user ID/Name:</label>
                 <select class="massmsgdrpdwn" id="notione">
-                    <?php
+                    <?php try {
                     /* Creating the dropdown empty option */
                     echo ('<option value = "0" disabled selected>'." ".'</option>');
                     /* SQL query to get the data from the DB */
@@ -31,15 +34,22 @@ try {
                     for ($i=0; $i<$DB->numRows(); $i++) { /* iterating the results by the num of rows */
                         /* Creating the dropdown options from DB */
                         echo ('<option value = "' . $x[$i]->id . '">' . $x[$i]->id . " - " . $x[$i]->fullname . '</option>');
-                    }?>
+                    }
+                }
+                catch(Exception $e)
+                {
+                    $_SESSION['error'] = 'error in sql';
+                    error_log("error in massmsgs page");
+                }?>
                 </select>
             </fieldset>
             <form method='post'> <!-- The notification form, functions are in backend.js using ajax -->
                 <br><legend>Notification content:</legend>
                 <textarea name="notification" rows="8" cols="50" class="massmsgfield" id="massnoti" required></textarea>
                 <br><br>
-                <input type="button" class="massmsgsendbtn" id="notisendone" value="Send to selected user">
-                <input type="button" class="massmsgsendbtn" id="notisendall" value="Send to all users">
+                <?php if ($_SESSION['error'] == 'error in sql') { echo "<div class='alert alert-danger' style='text-align: center;'>ERROR! Please try again later</div>"; } ?>
+                <input type="button" class="massmsgsendbtn" id="notisendone" value="Send to selected user" <?php if ($_SESSION['error'] == 'error in sql') { echo "style='display: none;'"; } ?>>
+                <input type="button" class="massmsgsendbtn" id="notisendall" value="Send to all users" <?php if ($_SESSION['error'] == 'error in sql') { echo "style='display: none;'"; } ?>>
             </form>
         </td>
         <td><div class = "vertical"></div><div class = "horizontal"></div></td>
@@ -51,7 +61,7 @@ try {
             <fieldset>
                 <label>Select the user ID/Name/Email:</label>
                 <select class="massmsgdrpdwn" id="mailone">
-                    <?php
+                    <?php try {
                     /* Creating the dropdown empty option */
                     echo ('<option value = "$i" disabled selected>'." ".'</option>');
                     /* SQL query to get the data from the DB */
@@ -62,7 +72,13 @@ try {
                     for ($i=0; $i<$DB->numRows(); $i++) { /* iterating the results by the num of rows */
                         /* Creating the dropdown options from DB */
                         echo ('<option value = "' . $x[$i]->email . '">' . $x[$i]->id . " - " . $x[$i]->fullname . " - " . $x[$i]->email . '</option>');
-                    }?>
+                    }
+                }
+                catch(Exception $e)
+                {
+                    $_SESSION['error'] = 'error in sql';
+                    error_log("error in massmsgs page");
+                }?>
                 </select>
             </fieldset>
             <form method='post'> <!-- The mail form, functions are in backend.js using ajax -->
@@ -73,19 +89,12 @@ try {
                 <br><legend>Mail content:</legend>
                 <textarea name="mailcontent" rows="8" cols="50" class="massmsgfield" id="mailcontent" required></textarea>
                 <br><br>
-                <input type="button" class="massmsgsendbtn" id="mailsendone" value="Send to selected user">
-                <input type="button" class="massmsgsendbtn" id="mailsendall" value="Send to all users">
+                <?php if ($_SESSION['error'] == 'error in sql') { echo "<div class='alert alert-danger' style='text-align: center;'>ERROR! Please try again later</div>"; } ?>
+                <input type="button" class="massmsgsendbtn" id="mailsendone" value="Send to selected user" <?php if ($_SESSION['error'] == 'error in sql') { echo "style='display: none;'"; } ?>>
+                <input type="button" class="massmsgsendbtn" id="mailsendall" value="Send to all users" <?php if ($_SESSION['error'] == 'error in sql') { echo "style='display: none;'"; } ?>>
             </form>
         </td>
     </tr>
 </table>
 
-<?php 
-}
-catch(Exception $e)
-{
-    echo "<br><div class='alert alert-danger' style='text-align: center;'>ERROR! Please try again later</div>";
-    $_SESSION['error'] = 'error in sql';
-    error_log("error in massmsgs page");
-}
-include "../template/footer.php"; /* Including the footer file */ ?>
+<?php include "../template/footer.php"; /* Including the footer file */ ?>

b997336809a095cc832a65607c43a5e30ecb4728
markRefaat <mark.refaat.ramzy@gmail.com>
2020-01-03 19:34:19 +0200

    Fix forget pass

--- operations/resetpass.php
+++ operations/resetpass.php
@@ -3,9 +3,9 @@ include('../DB/Database.php'); /* Including the DB */
 $DB = new Database(); /* Making a DB object */
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-	if(isset($_POST['type'] == "reset"))
-	{
-		try {
+    if(isset($_POST['type']) && $_POST['type'] == "reset")
+    {
+        try {
             $mail = filter_var($_POST['mail'], FILTER_SANITIZE_EMAIL);
             $password = $_POST['NewPass'];
             $sql = " SELECT * FROM employee WHERE email = '$mail' ";
@@ -13,9 +13,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $DB->execute();
             if($DB->numRows() > 0)
             {
-                $sql = " UPDATE employee SET password = " . sha1($password) . " WHERE email = '$mail' ";
+                $sql = " UPDATE employee SET password = '" . sha1($password) . "' WHERE email = '$mail' ";
                 $DB->query($sql);
                 $DB->execute();
+                echo "True";
             }
             else
             {
@@ -27,7 +28,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             echo "<div class='alert alert-danger'>Error please try again later</div>";
             error_log("error while resettin user password");
         }
-	}
+    }
 }
 else
 {

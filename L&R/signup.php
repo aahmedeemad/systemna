75715a65bc45e6b46aadf420c2c634a1290c8b70
fadybassel1 <56454311+fadybassel1@users.noreply.github.com>
2020-01-05 07:34:23 +0200

    signup wasn't working XD

--- L&R/signup.php
+++ L&R/signup.php
@@ -8,16 +8,16 @@ if($_SERVER['REQUEST_METHOD']=='POST'){
     $email = filter_var($_POST["Email"], FILTER_SANITIZE_EMAIL);
     $username = $_POST['username'];
     try{
-        $sql = "INSERT INTO employee VALUES (NULL, '" . $name . "' ,  '" . $username . "' , '" . sha1($_POST["password"]) . "' , '" . $email . "' , '" . $_POST['phone'] . "', '" . $_POST["ssn"] . "' , 2 , 1 , 'user')";
+        $sql = "INSERT INTO employee VALUES (NULL, '" . $name . "' ,  '" . $username . "' , '" . sha1($_POST["password"]) . "' , '" . $email . "' , '" . $_POST['phone'] . "', '" . $_POST["ssn"] . "' ,0000-00-00, 2 , 1 , 'user')";
         $DB->query($sql);
         $DB->execute();
         $lid = $DB->lastInsertedId();
-        $sql = "INSERT INTO add_info VALUES($lid , NULL , NULL , NULL, 0, 0, 0)";
+        $sql = "INSERT INTO add_info VALUES($lid , NULL , NULL ,'', NULL, 0, 0, 0)";
         $DB->query($sql);
         $DB->execute();
         $Message = "<p style='color:green;'>Signup Complete.<p>";
         header("Location:../index.php?Message={$Message}");
-    }
+   }
     catch (Exception $e) {
         $Message = "<p style='color:red;'>Opss..Something went wrong!<p>";
         header("Location:../index.php?Message={$Message}");
